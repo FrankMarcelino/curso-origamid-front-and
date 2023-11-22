13098ab056a8aafc8fdf062929846fa5f46128f1
frankMarcelino <frank.silva.ti@gmail.com>
2023-11-22 11:54:54 -0300

    Eventos 3

---
+++ JavaScript/exercio-seguir-mouse/script.js
@@ -0,0 +1,17 @@
+
+function seguirMouse(event) {
+
+    const x = event.x;
+    const y = event.y;
+    const circulo = document.querySelector('#circulo');
+    circulo.style.top = y + "px";
+    circulo.style.left = x + "px";
+
+}
+
+
+window.addEventListener('mousemove', seguirMouse)
+
+
+
+
