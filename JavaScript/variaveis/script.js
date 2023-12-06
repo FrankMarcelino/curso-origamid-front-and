64c32f6bcd6016451448a7c4c599a328d143c5a3
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-06 09:28:23 -0300

    variaveis

---
+++ JavaScript/variaveis/script.js
@@ -0,0 +1,13 @@
+const nome = "Frank"
+var idade = 31
+let comidaFavorita; 
+comidaFavorita = "hamburguer"
+
+var variavel1,
+    variavel2,
+    variavel3,
+    variavel4,
+    variavel5;
+
+
+console.log(nome, idade, comidaFavorita)
