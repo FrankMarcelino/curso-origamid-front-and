c3a9af344f6fd5c9cfd1c6cf1d3c87035523d1c8
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-06 15:13:54 -0300

    operadores e numero

---
+++ JavaScript/operadores-e-numeros/script.js
@@ -0,0 +1,29 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+// R: 35
+
+// Crie duas expressões que retornem NaN
+// var expresao1 = n1 / 1s
+// var expresao2 = n1 - "s1" 
+// console.log(typeof expresao2)
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var soma = +'200' + 50
+
+console.log(soma)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+var incremento = 5
+console.log(++incremento)
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = numero / 2; // NaN (Not a Number)
+var pesoDividido = pesoPorDois + unidade
+
+console.log(pesoDividido)
