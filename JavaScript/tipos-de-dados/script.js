1f40b15e20872af43433d730a88c55a8cd1262a6
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-06 09:47:26 -0300

    tipos de dados

---
+++ JavaScript/tipos-de-dados/script.js
@@ -0,0 +1,11 @@
+var variavelString = "string"
+var numeroString = "1"
+var minhaIdade = 31
+
+var nome = "Frank"
+var sobrenome = "Silva"
+var nomeCompleto = nome + ' ' + sobrenome
+var seguinteFrase = "it's time"
+
+console.log(variavelString, numeroString, minhaIdade, nome, sobrenome, nomeCompleto, seguinteFrase)
+console.log(typeof nome)
