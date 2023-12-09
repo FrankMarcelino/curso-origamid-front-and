89ec4f74791ac83205d7bad27c12449dbca83182
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-09 16:40:15 -0300

    ultimo comentario de hoje?

---
+++ JavaScript/tudo-e-objeto/script.js
@@ -0,0 +1,29 @@
+// nomeie 3 propriedades ou métodos de strings
+    var   nome = "frank"
+
+    // propriedade
+    nome.length
+    // metodo
+    nome.toUpperCase
+    nome.charAt
+    nome.charAt.prototype
+// nomeie 5 propriedades ou métodos de elementos do DOM
+    // propriedades
+    document.documentElement
+    document.onmouseover
+    document.images
+    document.forms
+    document.onclick
+
+
+    // metodos
+    document.createElement
+    document.querySelector
+    document.getElementById
+    document.querySelector
+    document.addEventListener
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+    ClipboardEvent.
