2cd44bc8d0ab494a6b2d1467c92c42ca39b83d0e
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-19 16:57:55 -0300

    navegação por tab

--- JavaScript/JS-DOM-iniciantes/Traversing-e-Manipulação/script.js
+++ JavaScript/JS-DOM-iniciantes/Traversing-e-Manipulação/script.js
@@ -1,30 +1,56 @@
-// Duplique o menu e adicione ele em copy
+// // Duplique o menu e adicione ele em copy
 
-const menu = document.querySelector('.menu');
-const novoMenu = menu;
-const cloneMenu = menu.cloneNode(true);
-const footer = document.querySelector('.copy');
-footer.appendChild(cloneMenu);
-// Selecione o primeiro DT da dl de Faq
+// const menu = document.querySelector('.menu');
+// const novoMenu = menu;
+// const cloneMenu = menu.cloneNode(true);
+// const footer = document.querySelector('.copy');
+// footer.appendChild(cloneMenu);
+// // Selecione o primeiro DT da dl de Faq
 
-let primeiroDT = document.querySelector('dl')
+// let primeiroDT = document.querySelector('dl')
 
-let primeiroFilho = primeiroDT.children[0]
+// let primeiroFilho = primeiroDT.children[0]
 
-let texto = primeiroFilho.outerText
+// let texto = primeiroFilho.outerText
 
-console.log(texto)
-// Selecione o DD referente ao primeiro DT
+// console.log(texto)
+// // Selecione o DD referente ao primeiro DT
     
-const dt = document.querySelector('dl')
-const dd = dt.children[1]
+// const dt = document.querySelector('dl')
+// const dd = dt.children[1]
 
-const primeiroDD = dd.outerText
+// const primeiroDD = dd.outerText
 
-console.log(primeiroDD)
-// Substitua o conteúdo html de .faq pelo de .animais
-const pai = document.querySelector('body')
+// console.log(primeiroDD)
+// // Substitua o conteúdo html de .faq pelo de .animais
+// const pai = document.querySelector('body')
+// const faq = document.querySelector('.faq')
+// const animais = document.querySelector('.animais')
+
+// pai.replaceChild(faq,animais)
+// ============================================================================
+
+//  CORREÇÃO DO PROFESSOR 
+
+// DUPLIQUE O MENU E ADICIONE ELE EM COPY 
+
+const menu  = document.querySelector('.menu')
+const copy = document.querySelector('.copy')
+
+const cloneMenu = menu.cloneNode(true)
+copy.appendChild(cloneMenu)
+
+// SELECIONE O PRIMEIRO DT DA DL DE FAQ
+
 const faq = document.querySelector('.faq')
+const primeiroDt = faq.querySelector('dt')
+
+// SELECONE O DD REFERENTE AO PRIMEIRO DT
+const proximoDD = primeiroDt.nextElementSibling
+
+console.log(proximoDD)
+
+// SUBSTITUA O CONTEÚDO HTML DE .FAQ PELO DE .ANIMAIS
 const animais = document.querySelector('.animais')
 
-pai.replaceChild(faq,animais)
+faq.innerHTML = animais.innerHTML
