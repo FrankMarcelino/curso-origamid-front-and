c0d95eddc3237cba30e3d8ec8382204d838bb1c2
frankMarcelino <frank.silva.ti@gmail.com>
2023-12-22 19:11:24 -0300

    native host and use

---
+++ JavaScript/JS-Objtos/native-host-use/script.js
@@ -0,0 +1,14 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Number
+Function
+// Liste 5 objetos do browser (host)
+NodeList
+HTMLCollection
+Element
+Window
+Document
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
