6af3162bb4e1ce942875c9e89801c90e61a36ff7
frankMarcelino <frank.silva.ti@gmail.com>
2024-01-07 14:21:37 -0300

    terminando exercicio de array

--- JavaScript/JS-Objtos/Array/srcript.js
+++ JavaScript/JS-Objtos/Array/srcript.js
@@ -18,9 +18,16 @@ console.log(comidas)
 
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
+console.log(estudantes.sort())
+
 // Inverta a ordem dos estudantes
+console.log(estudantes.reverse())
 // Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'))
+
 // Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'))
+
 
 let html = `<section>
               <div>Sobre</div>
@@ -30,6 +37,20 @@ let html = `<section>
 // Substitua section por ul e div com li,
 // utilizando split e join
 
+
+html = html.split('section');
+html = html.join('ul')
+html = html.split('div');
+html = html.join('li')
+
+console.log(html)
+
+
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+
+const cloneCarros = carros.slice()
+console.log(carros.pop())
+console.log(cloneCarros)
